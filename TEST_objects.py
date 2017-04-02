2841ec553bdae6f230bad42498457524d33a2e1c
Tristan Chambers <tristan.chambers@gmail.com>
2017-04-02 00:50:01 -0400

    New Part class

--- TEST_objects.py
+++ TEST_objects.py
@@ -9,7 +9,7 @@ class Tristate:
 
 class Measure:
     "A single measure, aka bar of musical notation"
-    def __init__(self, name, number_of_steps, default=Tristate.Off):
+    def __init__(self, name, number_of_steps=16, default=Tristate.Off):
         self.name = name
         self.content = []
         self.number_of_steps = number_of_steps
@@ -75,22 +75,50 @@ def export_measure(measure):
             raise
     return yaml.dump({measure.name: {'type':type(measure).__name__, 'content': flat_content}}, default_flow_style=False)
 
-def main():
-    mymeasure = Measure('mymeasure', 8)
-    myvariation = mymeasure.make_variation()
+class Stack(list):
+    def move(self, from_index, to_index):
+        self.insert(to_index, self.pop(from_index))
 
-    mymeasure.set_note(0, Tristate.On)
+class Part:
+    def __init__(self, number_of_bars=4):
+        self.name = ''
+        self.content = []
+        new_measure = Measure('blank measure')
+        for _ in range(number_of_bars):
+            self.content.append({'base_measure': new_measure, 'variations': Stack()})
+    def __repr__(self):
+        return pformat(self.content)
+
+    def set_base_measure(self, bar_number, measure):
+        if type(measure) == type(Measure('blank')):
+            self.content[bar_number]['base_measure'] = measure
+        else:
+            raise
+
+def main():
+    mypart = Part()
+    mybasemeasure = mypart.content[0]['base_measure']
+
+
+    mybasemeasure.set_note(0, Tristate.On)
+    mynewmeasure = Measure('other measure')
+    mypart.set_base_measure(2, mynewmeasure)
+    mynewmeasure.set_note(7,Tristate.On)
+    mynewmeasure.set_note(8,Tristate.On)
+
+    myvariation = mybasemeasure.make_variation()
     myvariation.set_note(0, Tristate.Off)
     myvariation.set_note(2, Tristate.On)
 
-    myproduct = merge_measures(mymeasure, myvariation)
+    myproduct = merge_measures(mybasemeasure, myvariation)
 
 #    output = open('data.pkl', 'wb')
 #    pickle.dump(mymeasure, output)
 #    output.close()
 
-    print(export_measure(mymeasure))
+    print(export_measure(mybasemeasure))
     print(export_measure(myvariation))
+    print(export_measure(myproduct))
     import pdb; pdb.set_trace()
 #    print(yaml.load(yaml.dump(mymeasure, default_flow_style=False)))
 
